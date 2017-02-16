8ce5a0d5d40fb35242a13a5d2361fcb3062435bf
xiaomeng <282044472@qq.com>
2017-02-16 17:07:28 +0800

    26-30章

---
+++ 5-技巧型设计模式/chapter_29数据访问对象模式/29.9node配置项.js
@@ -0,0 +1,11 @@
+
+// config.js
+// 将配置数据输出
+module.exports = {
+    // 数据库相关配置数据
+    DB : {
+        db : 'demo',        // 数据库名称
+        host : 'localhost',     // 主机名
+        port : 27017    // 端口号
+    }
+}
